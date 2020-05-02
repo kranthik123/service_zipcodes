bd14aeb6f21876d4cc1a0d07ad1b259430047d71
Kranthi Kavuri <kranthik123@gmail.com>
2020-05-01 23:27:23 -0500

    updated from zipcodes to products

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,32 +1,13 @@
 from flask import Flask
 from flask import make_response
-import json
 from flask import jsonify
-from flask import request
-import re
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    zipcode_list = []
-    with open('zipcodes.json') as json_file:
-        contents = json.load(json_file)
-    for l in contents['data']:
-        zipcode_list.append(l['zipcode'])
-    return jsonify(zipcode_list)
-
-
-@app.route('/zipcode')
-def zipcode():
-    zipc = request.args.get('zipcode')
-    pattern = "^[0-9]{5}(?:-[0-9]{4})?$"
-    if re.match(pattern, zipc):
-        response = make_response('Valid zipcode entered: %s.' % zipc)
-        return response
-    else:
-        response = make_response('Invalid zipcode provided: %s.' % zipc)
-        return response
+    products_list = ["Rain Coat", "Rain Boots"]
+    return jsonify(products_list)
 
 @app.route('/<page_name>')
 def other_page(page_name):
