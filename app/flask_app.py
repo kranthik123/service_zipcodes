78059dca0c1a9f95b1586571dadaa3481dd7b5c5
Kranthi Kavuri <kranthik123@gmail.com>
2020-05-03 10:24:31 -0500

    Added CORS

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import make_response
 from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
 def main():
